6d18ec0b8792edcd244dd53ec39c314b7e5883cc
Kaitlyn Long <katielong11@gmail.com>
2022-01-27 14:44:41 -0600

    a user can now create a book

--- Ryans-World/Ryans-World/Controllers/BookController.cs
+++ Ryans-World/Ryans-World/Controllers/BookController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ryans_World.Models;
 using Ryans_World.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Ryans_World.Controllers
@@ -13,16 +16,35 @@ namespace Ryans_World.Controllers
     public class BookController : ControllerBase
     {
         private readonly IBookRepository _bookRepository;
+        private readonly IUserProfileRepository _userProfileRepository;
 
-        public BookController(IBookRepository bookRepository)
+        public BookController(IBookRepository bookRepository, IUserProfileRepository userProfileRepository)
         {
             _bookRepository = bookRepository;
+            _userProfileRepository = userProfileRepository;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
             return Ok(_bookRepository.GetAll());
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            var currentUserProfile = GetCurrentUserProfile();
+            book.UserProfileId = currentUserProfile.Id;
+            book.CategoryId = 1;
+            _bookRepository.Add(book);
+            return CreatedAtAction("Get", new { id = book.Id }, book);
+        }
+
+        private UserProfile GetCurrentUserProfile()
+        {
+            var fireBaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(fireBaseUserId);
         }
     }
 }
