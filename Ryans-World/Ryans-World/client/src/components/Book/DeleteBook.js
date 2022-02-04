d64024e6ece198a20fd858325ee33b7274243a07
Kaitlyn Long <katielong11@gmail.com>
2022-02-04 13:45:33 -0600

    completed book detail css

--- Ryans-World/Ryans-World/client/src/components/Book/DeleteBook.js
+++ Ryans-World/Ryans-World/client/src/components/Book/DeleteBook.js
@@ -15,11 +15,12 @@ const DeleteBook = ({ }) => {
     };
 
     return (
-        <div>
-            <h2> Delete Book:</h2>
-            <p>Are you sure you want to delete this book?</p>
-            <button onClick={handleDeleteBook}>Delete</button>
-            <button onClick={handleCancelDelete}>Cancel</button>
+        <div className="DeleteBookForm">
+            <p className="TextBook">- Are you sure you want to delete this book? -</p>
+            <ul className="DeleteBookButtons">
+                <li><button className="ConfirmDeleteBook" onClick={handleDeleteBook}><p className="BookDelete">Delete</p></button></li>
+                <li><button className="ConfirmCancelBook" onClick={handleCancelDelete}><p className="BookCancel">Cancel</p></button></li>
+            </ul>
         </div>
     )
 }
